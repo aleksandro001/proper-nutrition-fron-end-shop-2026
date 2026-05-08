0b58255cc27f5b7484d95a8e61fba210132f76e8
aleksandro001 <001aleksandro@gmail.com>
2026-05-08 09:39:22 +0300

    add  refresh tokens middleware (proxy)

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,27 @@
 import { PAGES } from './shared/config/page.config'
+import { getTokens } from './shared/lib/server/get-tokens.server'
+import { jwtVerifyServer } from './shared/lib/server/jwt-verify.server'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function proxy(reg: NextRequest, res: NextResponse) {
-  if (!reg.cookies.get('accessToken')) {
+  const tokens = await getTokens(reg)
+
+  if (!tokens) {
     return NextResponse.redirect(new URL(PAGES.LOGIN, reg.url))
   }
+
+  if ('isRefreshedToken' in tokens) {
+    const response = NextResponse.next()
+    if (tokens.setCookie) {
+      response.headers.set('set-cookie', tokens.setCookie)
+    }
+    return response
+  }
+  const verifyResult = await jwtVerifyServer(tokens.accessToken)
+  if (!verifyResult) {
+    return NextResponse.redirect(new URL(PAGES.LOGIN, reg.url))
+  }
+  return NextResponse.next()
 }
 
 export const config = {
